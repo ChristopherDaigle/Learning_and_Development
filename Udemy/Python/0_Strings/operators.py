69f0d9d7be45f79753924cb77c169cc839bb0135
Chris Daigle <chris.daigle@uconn.edu>
2020-01-05 15:01:25 -0500

    feat: replacement field formatting
    work through course

--- Udemy/Python/0_Strings/operators.py
+++ Udemy/Python/0_Strings/operators.py
@@ -1,13 +1,13 @@
 # Python has three numeric types:
 #
 # 1. int: -1
-## Python 3 has no integer max size
+# Python 3 has no integer max size
 #
 # 2. float: -1.0
-## Max value is: 1.7976931348623157e+308
-## Min value: 2.2250738585072014e-308
-## 52 digits of precision
-## Python 3 also has a "Decimal" data type which is more precise
+# Max value is: 1.7976931348623157e+308
+# Min value: 2.2250738585072014e-308
+# 52 digits of precision
+# Python 3 also has a "Decimal" data type which is more precise
 #
 # 3. complex: i ** 2
 
@@ -32,3 +32,12 @@ print()
 # Follow's order of operations
 print(a + b / 3 - 4 * 12)
 print(a + (b / 3) - (4 * 12))
+print()
+
+# Sequence: ORDERED set of items
+# "Hello World" is a sequence of 11 items, each are string characters
+# ['computer', 'monitor', 'keyboard', 'mouse', 'mouse pad'] is a sequence of 5 items, each are strings, which are
+# also sequences themselves - a list may be a sequence of sequences
+computer_parts = ['computer', 'monitor', 'keyboard', 'mouse', 'mouse pad']
+print(computer_parts[1])   # 'monitor'
+print(computer_parts[1][0])    # 'm'
